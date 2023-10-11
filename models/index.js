a312c8d33d39e566d678de672bd9b42c5ec2ed80
Raphael Lima <rapha.lima.rl16@gmail.com>
2023-10-11 00:45:58 -0300

    Subindo a aplicacao e criando o primeiro controller

---
+++ models/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const PokemonSchema = require('/pokemon');
+
+const Pokemon = mongoose.model('Pokemon', PokemonSchema);
+
+const connect = () => {
+    mongoose.connect('mongodb://127.0.0.1:27017/pokedex');
+};
+
+module.exports = {
+    Pokemon,
+    connect
+};
