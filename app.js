1dd155132989fc135cfb0d99a6f26e63fd39e2c3
Raphael Lima <rapha.lima.rl16@gmail.com>
2023-10-11 22:26:10 -0300

    Fazendo minha aplicacao conversar com a pokeApi, utilizando o axios

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
+
 const expressLayouts = require('express-ejs-layouts');
 const { connect } = require('./models');
 
 const pokemonsRouter = require('./routes/pokemons');
-
+const batalhaRouter = require('./routes/batalha');
 const app = express();
 
 // configurando ejs
@@ -12,8 +13,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
+//configurando arquivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
+
 // declarando rotas
 app.use('/pokemons', pokemonsRouter);
+app.use('/batalha', batalhaRouter);
 
 const porta = 3000;
 app.listen(porta, () => {
