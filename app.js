680c26f4cfa33bd493f129a3923226a5c7cffc26
Raphael Lima <rapha.lima.rl16@gmail.com>
2023-10-16 21:33:10 -0300

    Configurando os erros previstos

--- app.js
+++ app.js
@@ -25,6 +25,20 @@ app.use('/batalha', batalhaRouter);
 // declarando rotas de api
 app.use('/api', capturaRouter);
 
+// caso nao de match em nenhuma, tratamos o 404
+app.use((_req, _res, next) => {
+    next(createError(404));
+});
+
+// tratativa de error
+app.use((err, _req,res, _next) => {
+    res.status(err.status || 500);
+    res.render('paginas/erro', {
+        mensagem: err.message,
+        erro: err,
+    });
+});
+
 const porta = 3005;
 app.listen(porta, () => {
     connect();
